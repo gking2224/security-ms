f41841a45974ac702b6756f6d3ce27344de09c21
Graham King <gking2224@gmail.com>
2016-10-13 14:43:37 +0100

    abstracted common dao/service logic

--- src/main/java/me/gking2224/securityms/db/dao/UserDao.java
+++ src/main/java/me/gking2224/securityms/db/dao/UserDao.java
@@ -1,20 +1,8 @@
 package me.gking2224.securityms.db.dao;
 
-import java.util.List;
-
+import me.gking2224.common.db.dao.CrudDao;
 import me.gking2224.securityms.model.User;
 
-public interface UserDao {
-
-    User create(User user);
-
-    List<User> findAll();
-
-    User update(User user);
-
-    void delete(Long id);
-
-    User findById(Long id);
-
+public interface UserDao extends CrudDao<User, Long>{
 
 }
--- src/main/java/me/gking2224/securityms/db/dao/UserDaoImpl.java
+++ src/main/java/me/gking2224/securityms/db/dao/UserDaoImpl.java
@@ -1,10 +1,7 @@
 package me.gking2224.securityms.db.dao;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jmx.export.annotation.ManagedAttribute;
-import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,52 +9,18 @@ import me.gking2224.common.db.AbstractDaoImpl;
 import me.gking2224.securityms.db.jpa.UserRepository;
 import me.gking2224.securityms.model.User;
 
-@ManagedResource(objectName="dao:name=thingDaoImpl", description="UserDao", log=true,
-logFile="jmx.log", currencyTimeLimit=15, persistPolicy="Never")
 @Component
 @Transactional
-public class UserDaoImpl extends AbstractDaoImpl<User> implements UserDao {
+public class UserDaoImpl extends AbstractDaoImpl<User, Long> implements UserDao {
 
     @Autowired
     protected UserRepository userRepository;
-    
-    private int lastLoadCount = 0;
     
     public UserDaoImpl() {
     }
 
     @Override
-    public User create(User user) {
-        User saved = userRepository.save(user);
-        return saved;
-    }
-
-    @Override
-    public List<User> findAll() {
-        List<User> users = userRepository.findAll();
-        this.lastLoadCount = users.size();
-        return users;
-    }
-
-    @Override
-    public User update(User user) {
-        User saved = userRepository.save(user);
-        return saved;
-    }
-    
-    @Override
-    public void delete(Long id) {
-        userRepository.delete(id);
-    }
-
-    @Override
-    public User findById(Long id) {
-        User user = userRepository.findOne(id);
-        return user;
-    }
-
-    @ManagedAttribute
-    public int getLastLoadCount() {
-        return lastLoadCount;
+    protected JpaRepository<User, Long> getRepository() {
+        return userRepository;
     }
 }
